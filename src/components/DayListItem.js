662cdffe7fbc7cfeb5da8d4be9f0988294750b94
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-07-13 17:17:33 -0600

    match spots display to pre witten test result on DayListItem.test.js

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,7 +4,14 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
 
-  //const [ spots, setSpots ] = props;
+const formatSpots = function(spots) { //creating dispaly msg to pass pre-written tests for DayListItem
+if (spots === 1)
+  return spots + " spot remaining";
+else if (spots > 1)
+  return spots + " spots remaining";
+else if (!spots)
+  return "no spots remaining";
+}
 
   const dayClass = classNames("day-list__item", { 
     "day-list__item--selected": props.selected, 
@@ -15,7 +22,9 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
+
+
