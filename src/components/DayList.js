1a7b7950b74755d836c73e0d5bc15800ad1e58c7
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-07-14 16:38:54 -0600

    add dayList inside DayListItem

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,6 +3,6 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
 return ( <ul>
-  {props.days.map(day => <DayListItem key={day.id} setDay selected={day.name === props.day}/>)}
+  {props.days.map(day => <DayListItem key={day.id} name={day.name} spots={day.spots} setDay={props.setDay} selected={day.name === props.day}/>)}
 </ul>)
 }
