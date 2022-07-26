7419340261fbfa97adf888ea640b906dc3af5212
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-07-26 14:29:12 -0600

    add CREATE transition

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -19,7 +19,7 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={props.onAdd}/>}
+      {mode === EMPTY && <Empty onAdd={transition(CREATE)}/>}
       {mode === SHOW && <Show
           student={props.interview.student}
           interviewerName={props.interview.interviewer.name}
