e1d4f19087cd250546bcb8779a720309e21b74eb
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-07-26 14:26:05 -0600

    conditional render of components using useVisualmode custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,16 +5,25 @@ import './styles.scss';
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "../../hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
+export default function Appointment(props) {
 
 
-export default function Appointment(props){
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      { props.interview ? 
-        <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/> 
-        : <Empty/> 
+      {mode === EMPTY && <Empty onAdd={props.onAdd}/>}
+      {mode === SHOW && <Show
+          student={props.interview.student}
+          interviewerName={props.interview.interviewer.name}
+        />
       }
     </article>
   );
